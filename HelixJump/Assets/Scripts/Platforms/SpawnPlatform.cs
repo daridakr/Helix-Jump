96bdab5b5fb5d33694319b55040a6f7a43bef3ca
Krivushkina Daria <daridakr@gmail.com>
2022-08-03 16:02:29 +0300

    Spawn a ball

--- HelixJump/Assets/Scripts/Platforms/SpawnPlatform.cs
+++ HelixJump/Assets/Scripts/Platforms/SpawnPlatform.cs
@@ -4,4 +4,9 @@ public class SpawnPlatform : Platform
 {
     [SerializeField] private Ball _ball;
     [SerializeField] private Transform _spawnPoint;
+
+    private void Awake()
+    {
+        Instantiate(_ball, _spawnPoint.position, Quaternion.identity);
+    }
 }
